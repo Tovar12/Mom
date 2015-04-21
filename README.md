066885db860aebf64b063d3f819412d8af7099d9
Felipe Tovar <f.tovar12@hotmail.com>
2015-04-21 18:09:17 -0500

    User almost complete

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 1. Run active console:
+
+
   1.1. go apache-activemq-5.10.0/bin
+
+
   1.2. ./activemq console
 2. Compile:
   javac -cp lib/activemq.jar User.java
--- User.java
+++ User.java
@@ -11,54 +11,33 @@ import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.jms.Topic;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class User implements ExceptionListener {
   private Session session;
-  public void processConsumer(int option)throws Exception{
+  private Connection connection;
+  private Destination destination;
+  private MessageConsumer consumer;
+  private Boolean isListening = true;
+  private String userTopic;
+  private Scanner scanner = new Scanner (System.in);
 
+//Here we create the connection and subscribe the user to a default topic
+  public void processConsumer() throws Exception{
     // Create a ConnectionFactory
     ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
             "tcp://localhost:61616");
-
     // Create a Connection
-    Connection connection = connectionFactory.createConnection();
-    //connection.start();
-
+    connection = connectionFactory.createConnection();
     connection.setExceptionListener(this);
-
     // Create a Session
     session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-    //Destination destination = session.createTopic("MyTopic");
-    //MessageConsumer consumer = session.createConsumer(destination);
-    //consumer.setMessageListener(null);
-    //destination = session.createTopic(channelName);
-    //consumer = session.createConsumer(destination);
-    System.out.println("Entro al metodo");
-    switch(option){
-      case 1: System.out.println("Listening Cars");
-              Destination destination1 = session.createTopic("Cars");
-              MessageConsumer consumer1 = session.createConsumer(destination1);
-              consumer1.setMessageListener(listener);
-              connection.start();
-              break;
-      case 2: Destination destination2 = session.createTopic("Sports");
-              MessageConsumer consumer2 = session.createConsumer(destination2);
-              consumer2.setMessageListener(listener);
-              connection.start();
-              break;
-      case 3: Destination destination3 = session.createTopic("Money");
-              MessageConsumer consumer3 = session.createConsumer(destination3);
-              consumer3.setMessageListener(listener);
-              connection.start();
-              break;
-    }
-
-    //consumer1.close();
-    //session.close();
-    //connection.close();
-
+    System.out.println("You have been subscribed to our default topic");
+    destination = session.createTopic("default");
+    consumer = session.createConsumer(destination);
+    userTopic = "default";
+    receiveInput();
   }
 
   MessageListener listener = new MessageListener() {
@@ -73,13 +52,65 @@ public class User implements ExceptionListener {
                   e.printStackTrace();
               }
               System.out.println("Message: " + text);
-              System.out.println("------------------------");
           } else {
               System.out.println("Message: " + msg);
-              System.out.println("------------------------");
           }
       }
   };
+  /**
+  This method has a menu that allow the user between listen a new topic, stop
+  listening a topic or start listening again the topic.
+  **/
+  public void receiveInput() throws Exception{
+    while(true){
+      System.out.println("Menu: ");
+      System.out.println("1. Listen new topic");
+      //Evaluate if the user is available to listen the message of his topic.
+      if(isListening){
+        System.out.println("2. TurnOff connection");
+      }else{
+        System.out.println("2. TurnOn connection");
+      }
+      //Wait the input of the user and if the
+      switch(scanner.nextInt()){
+        case 1: listenNewTopic();
+                break;
+        case 2: turnOnOrOff();
+                break;
+        default: System.out.println("Input error");
+        }
+    }
+
+  }
+
+  public void listen()throws Exception{
+    consumer.setMessageListener(listener);
+    connection.start();
+  }
+
+  public void listenNewTopic() throws Exception{
+    System.out.println("Write the name of a topic you would like to subscribe:");
+    userTopic = scanner.next();
+    consumer.setMessageListener(null);
+    destination = session.createTopic(userTopic);
+    consumer = session.createConsumer(destination);
+    System.out.println("_________________________________________");
+    System.out.println("You are now listening *" + userTopic + "*");
+    System.out.println("_________________________________________");
+    listen();
+  }
+
+  public void turnOnOrOff() throws Exception {
+    if(isListening){
+      System.out.println("Connection turned off");
+      isListening = false;
+      consumer.setMessageListener(null);
+    }else{
+      System.out.println("Connection turned on");
+      isListening = true;
+      consumer.setMessageListener(listener);
+    }
+  }
 
   public synchronized void onException(JMSException ex) {
       System.out.println("JMS Exception occured.  Shutting down client.");
@@ -87,20 +118,6 @@ public class User implements ExceptionListener {
 
   public static void main(String[] args) throws Exception {
     User p = new User();
-    System.out.println("Select the topic you would like to recieve info");
-    System.out.println("1. Cars");
-    System.out.println("2. Sports");
-    System.out.println("3. Money");
-    Scanner scanner = new Scanner(System.in);
-    switch(scanner.nextInt()){
-      case 1: p.processConsumer(1);
-              System.out.println("ENTRO SWITCH");
-              break;
-      case 2: p.processConsumer(2);
-              break;
-      case 3: p.processConsumer(3);
-              break;
-      default: System.out.println("Input error");
-      }
+    p.processConsumer();
     }
   }
