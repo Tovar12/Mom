8a080863da2463250806ac3287fff016ad1ba8aa
Felipe Tovar <f.tovar12@hotmail.com>
2015-04-22 18:23:04 -0500

    the rss create dynamic topics

--- Rss.java
+++ Rss.java
@@ -12,137 +12,125 @@ import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.jms.Topic;
 import java.util.Scanner;
-
-
+import java.util.ArrayList;
 
 public class Rss implements ExceptionListener {
 
+  private ArrayList<String> topicsAvailable = new ArrayList<String>();
+  private Scanner scanner = new Scanner(System.in);
   private Session session;
-  public void processProducer(int option){
+  private Destination destination;
+  private MessageProducer producer;
+  private static Rss instance;
+
+  private Rss() {
+  }
+
+  public void processProducer(){
     try{
       // Create a ConnectionFactory
-			ActiveMQConnectionFactory connectionFactory = new
+      ActiveMQConnectionFactory connectionFactory = new
       ActiveMQConnectionFactory("tcp://localhost:61616");
 
-			// Create a Connection
-			Connection connection = connectionFactory.createConnection();
-			connection.start();
+      // Create a Connection
+      Connection connection = connectionFactory.createConnection();
+      connection.start();
 
-			// Create a Session
-			session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+      // Create a Session
+      session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
       //Default
-      Destination destination = session.createTopic("MyTOPIC");
-      MessageProducer producer = session.createProducer(destination);
+      destination = session.createTopic("default");
+      producer = session.createProducer(destination);
+      startProducing();
+      // Clean up
+      //session.close();
+      //connection.close();
+    } catch (Exception e) {
+      System.out.println("Caught: " + e);
+      e.printStackTrace();
+    }
+  }
 
-      switch(option){
-        case 1: Destination destination1 = session.createTopic("Cars");
-                MessageProducer producer1 = session.createProducer(destination1);
-                sendMessage1(producer1);
+  public void startProducing() throws Exception{
+    while(true){
+      System.out.println("Menu:");
+      System.out.println("1. Create a new topic");
+      System.out.println("2. Topics available");
+      System.out.println("3. Send a Message");
+      System.out.println("4. Delete a topic");
+      System.out.println("5. Exit");
+      switch(scanner.nextInt()){
+        case 1: createNewTopic();
                 break;
-        case 2: Destination destination2 = session.createTopic("Sports");
-                MessageProducer producer2 = session.createProducer(destination2);
-                sendMessage2(producer2);
+        case 2: getTopicsAvailable();
                 break;
-        case 3: Destination destination3 = session.createTopic("Money");
-                MessageProducer producer3 = session.createProducer(destination3);
-                sendMessage3(producer3);
+        case 3: sendMessage();
+                break;
+        case 4: deleteTopic();
+                break;
+        case 5: System.exit(1);
                 break;
         default: System.out.println("Input error");
-
       }
-      // Clean up
-			session.close();
-			connection.close();
-
-
-    } catch (Exception e) {
-			System.out.println("Caught: " + e);
-			e.printStackTrace();
-		}
-
-
-  }
-
-  public void sendMessage1(MessageProducer producer1) throws Exception{
-    for(int i = 0; i < 20; i++){
-      // Create a messages
-      String text = "Hola Mundo!" + i;
-      TextMessage message = session.createTextMessage(text);
-      // Tell the producer to send the message
-      System.out.println("Mensaje Enviado Cars: " + text);
-      //producer.send(message);
-      producer1.send(message);
     }
   }
-  public void sendMessage2(MessageProducer producer2)throws Exception{
-    for(int i = 0; i < 20; i++){
-      // Create a messages
-      String text = "Hola Mundo!" + i;
-      TextMessage message = session.createTextMessage(text);
-      // Tell the producer to send the message
-      System.out.println("Mensaje Enviado Sports: " + text);
-      //producer.send(message);
-      producer2.send(message);
+
+  public void createNewTopic(){
+    System.out.println("Write the name of new topic");
+    String newTopic = scanner.next();
+    topicsAvailable.add(newTopic);
+    System.out.println("The new topic with name " + newTopic +
+                                                        " has been created");
+  }
+
+  public void getTopicsAvailable(){
+    for(int i = 0; i < topicsAvailable.size(); i++){
+      System.out.println(topicsAvailable.get(i));
     }
   }
-  public void sendMessage3(MessageProducer producer3)throws Exception{
-    for(int i = 0; i < 20; i++){
-      // Create a messages
-      String text = "Hola Mundo!" + i;
-      TextMessage message = session.createTextMessage(text);
-      // Tell the producer to send the message
-      System.out.println("Mensaje Enviado Money: " + text);
-      //producer.send(message);
-      producer3.send(message);
+
+  public void sendMessage() throws Exception{
+    System.out.println("Select the topic:");
+    for(int i = 0; i < topicsAvailable.size(); i++){
+      System.out.println((i+1) + ". " + topicsAvailable.get(i));
+    }
+    String topicSelected = topicsAvailable.get(scanner.nextInt()-1);
+    destination = session.createTopic(topicSelected);
+    producer = session.createProducer(destination);
+    System.out.println("Write the message:");
+    String inputMessage = scanner.next();
+    TextMessage message = session.createTextMessage(inputMessage);
+    System.out.println("How many times would you like to send the message:");
+    int times = scanner.nextInt();
+    for(; times > 0; times--)  producer.send(message);
+    System.out.println("The message " + inputMessage + " has been sent");
+  }
+
+  public void deleteTopic(){
+    if(topicsAvailable.size() != 0){
+      System.out.println("These are the topics availables, write the name of the"+
+                                  " topic you want to delete");
+      getTopicsAvailable();
+      String option = scanner.next();
+      topicsAvailable.remove(option);
+    } else {
+      System.out.println("You don't have topics created");
     }
   }
 
   public synchronized void onException(JMSException ex) {
-		System.out.println("JMS Exception occured.  Shutting down client.");
-	}
-
-	public static void main(String[] args) throws Exception {
-		Rss p = new Rss();
-    System.out.println("Select the rss you would like to recieve info");
-    System.out.println("1. Cars");
-    System.out.println("2. Sports");
-    System.out.println("3. Money");
-    Scanner scanner = new Scanner(System.in);
-    switch(scanner.nextInt()){
-      case 1: p.processProducer(1);
-              break;
-      case 2: p.processProducer(2);
-              break;
-      case 3: p.processProducer(3);
-              break;
-      default: System.out.println("Input error");
-
-    }
-	}
-}
-
-
-
-/**
-
-public void Productor {
-  private static Productor instace;
-
-  private Productor() {
-
+    System.out.println("JMS Exception occured.  Shutting down client.");
   }
 
-  public static Productor getInstance() {
-    if (instance == null) instance = new Productor();
+  public static Rss getInstance() {
+    if (instance == null){
+      instance = new Rss();
+    }
     return instance;
   }
-}
 
-public class Main {
-  public static void main (String [] args) {
-    Productor miProductor = Productor.getInstance();
-    miProductor.miMetodo();
+  public static void main(String[] args) throws Exception {
+    Rss rss = Rss.getInstance();
+    rss.processProducer();
   }
 }
-
-**/
